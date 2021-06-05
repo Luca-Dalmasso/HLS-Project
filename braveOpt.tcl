cce048081ae550a6c861641cb2a4c97a5dc35807
alessandrolandra <77108854+alessandrolandra@users.noreply.github.com>
2021-06-05 09:05:29 +0200

    feat: braveOpt added

---
+++ braveOpt.tcl
@@ -0,0 +1,24 @@
+proc brave_opt args {
+  array set options {-total_area 0}
+  if { [llength $args] != 2 } {
+    return -code error "Use brave_opt with -total_area \$area_value\$"
+  }
+  foreach {opt val} $args {
+    if {![info exist options($opt)]} {
+      return -code error "unknown option \"$opt\""
+    }
+    set options($opt) $val
+  }
+  set total_area $options(-total_area)
+
+  puts $total_area
+  
+	source contest2.tcl
+	get_total_scheduling
+
+	#################################
+	### INSERT YOUR COMMANDS HERE ###
+	#################################
+
+	return
+}
